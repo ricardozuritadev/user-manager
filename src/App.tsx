89f63eaa0f773c7ffbf918a768a9134d93f6a27d
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-28 08:56:35 +0100

    Create users array data

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+const USERS = [
+  {
+    username: 'pablo',
+    name: 'Pablo Castellanos',
+    active: true,
+    role: 'teacher',
+  },
+  {
+    username: 'jose',
+    name: 'Jose Fernándex',
+    active: true,
+    role: 'teacher',
+  },
+  {
+    username: 'javier',
+    name: 'Javier López',
+    active: false,
+    role: 'student',
+  },
+];
+
 function App() {
   return <div className="App"></div>;
 }
