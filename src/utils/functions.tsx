d49161a85d9d0f39651e80e38f7eaa6dafe05b4d
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-28 13:30:54 +0100

    Move filters functions

--- src/utils/functions.tsx
+++ src/utils/functions.tsx
@@ -7,3 +7,32 @@ export const paintUsers = (users: User[]) => {
 
   return users.map(user => <UserRow key={user.username} {...user} />);
 };
+
+export const filterUsersByName = (users: User[], search: string) => {
+  if (!search) return [...users];
+
+  const lowerCasedSearch = search.toLowerCase();
+  return users.filter(user =>
+    user.name.toLowerCase().includes(lowerCasedSearch)
+  );
+};
+
+export const filterActiveUsers = (users: User[], active: boolean) => {
+  if (!active) return [...users];
+
+  return users.filter(user => user.active);
+};
+
+export const sortUsers = (users: User[], sortBy: number) => {
+  const sortedUsers = [...users];
+  switch (sortBy) {
+    case 1:
+      return sortedUsers.sort((a, b) => {
+        if (a.name > b.name) return 1;
+        if (a.name < b.name) return -1;
+        return 0;
+      });
+    default:
+      return sortedUsers;
+  }
+};
