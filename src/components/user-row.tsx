df7e0d0d42820033278a1fd058de12af347a2b59
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-28 13:30:14 +0100

    Modify user row component

--- src/components/user-row.tsx
+++ src/components/user-row.tsx
@@ -1,13 +1,20 @@
 import { Role, User } from '../types/user.type';
 
 const UserRow = ({ username, name, active, role }: User) => {
-  return (
-    <div>
-      <p>{username}</p>
-      <p>{name}</p>
-      <p>{role}</p>
+  <div className="c-user-row">
+    <div className="c-user-row__name">
+      <span>{name}</span>
     </div>
-  );
+    <div className="c-user-row__status">
+      <UserStatus active={active} />
+    </div>
+    <div className="c-user-row__role">
+      <UserRole role={role} />
+    </div>
+    <div className="c-user-row__action">
+      <button onClick={() => {}}>{active ? 'Desactivar' : 'Activar'}</button>
+    </div>
+  </div>;
 };
 
 export default UserRow;
