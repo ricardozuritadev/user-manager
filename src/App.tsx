a9fbe803286f8dfa135ceae50257b112000b3732
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-28 12:21:37 +0100

    Modify app cpmponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
+import { Role } from './types/user.type';
+
+import UsersList from './components/users-list';
+
 const USERS = [
   {
     username: 'pablo',
     name: 'Pablo Castellanos',
     active: true,
-    role: 'teacher',
+    role: Role.teacher,
   },
   {
     username: 'jose',
     name: 'Jose Fernándex',
     active: true,
-    role: 'teacher',
+    role: Role.teacher,
   },
   {
     username: 'javier',
     name: 'Javier López',
     active: false,
-    role: 'student',
+    role: Role.student,
   },
 ];
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <UsersList users={USERS} />
+    </div>
+  );
+};
 
 export default App;
