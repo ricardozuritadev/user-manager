f2d4cbe99affb3f91c862a545ead3dcc28006277
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-28 08:56:17 +0100

    Create user types

---
+++ src/types/user.type.ts
@@ -0,0 +1,12 @@
+enum Role {
+  teacher = 'teacher',
+  student = 'student',
+  other = 'other',
+}
+
+export type User = {
+  username: string;
+  name: string;
+  active: boolean;
+  role: Role;
+};
