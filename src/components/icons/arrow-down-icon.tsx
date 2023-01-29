632c042a481a206370f90d95ec1f95fd26fe0201
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-29 20:04:10 +0100

    Create ArrowDownIcon component

---
+++ src/components/icons/arrow-down-icon.tsx
@@ -0,0 +1,22 @@
+type ArrowDownIconProps = {
+  className: string;
+};
+
+const ArrowDownIcon = ({ className }: ArrowDownIconProps) => (
+  <svg
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M19 9l-7 7-7-7"
+    ></path>
+  </svg>
+);
+
+export default ArrowDownIcon;
