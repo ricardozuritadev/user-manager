1ad9cae5e877f543b92ebc1f9246a647add163c1
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-29 09:58:51 +0100

    Delete toggleUserActive function

--- src/hooks/use-users.tsx
+++ src/hooks/use-users.tsx
@@ -5,16 +5,5 @@ import { User } from '../types/user.type';
 export const useUsers = (initialUsers: User[]) => {
   const [users, setUsers] = useState(initialUsers);
 
-  const toggleUserActive = (userId: number) => {
-    const newUsers = structuredClone(users);
-
-    const userIndex = newUsers.findIndex(user => user.id === userId);
-    if (userIndex === -1) return;
-
-    newUsers[userIndex].active = !newUsers[userIndex].active;
-
-    setUsers(newUsers);
-  };
-
-  return { users, toggleUserActive };
+  return { users };
 };
