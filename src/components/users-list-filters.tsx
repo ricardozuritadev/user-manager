e2753022ec92e91d8c8a778a6be8e766ba1b7fce
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-29 12:26:54 +0100

    Add sort options

--- src/components/users-list-filters.tsx
+++ src/components/users-list-filters.tsx
@@ -39,6 +39,8 @@ const UsersListFilters = ({
       >
         <option value={0}>Por defecto</option>
         <option value={1}>Por nombre</option>
+        <option value={2}>Por role</option>
+        <option value={3}>Por estado</option>
       </select>
     </form>
   );
