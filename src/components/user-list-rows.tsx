ae1a864e9f80b648464456751e65f849eb25cbf8
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-29 09:57:59 +0100

    Delete toggleUSerActive prop

--- src/components/user-list-rows.tsx
+++ src/components/user-list-rows.tsx
@@ -4,16 +4,15 @@ import UserRow from '../components/user-row';
 
 type UserListRowsProps = {
   users: User[];
-  toggleUserActive: (userId: number) => void;
 };
 
-const UserListRows = ({ users, toggleUserActive }: UserListRowsProps) => {
+const UserListRows = ({ users }: UserListRowsProps) => {
   if (users.length <= 0) return <p>No hay usuarios</p>;
 
   return (
     <>
       {users.map(user => (
-        <UserRow key={user.username} {...{ user, toggleUserActive }} />
+        <UserRow key={user.username} {...user} />
       ))}
     </>
   );
