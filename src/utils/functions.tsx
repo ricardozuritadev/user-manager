1fb2b8618bf33db24fadb39168074a6e4777caff
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-29 12:27:15 +0100

    Implement new sort logic

--- src/utils/functions.tsx
+++ src/utils/functions.tsx
@@ -24,6 +24,20 @@ export const sortUsers = (users: User[], sortBy: number) => {
         if (a.name < b.name) return -1;
         return 0;
       });
+    case 2:
+      return sortedUsers.sort((a, b) => {
+        if (a.role === b.role) return 0;
+        if (a.role === 'teacher') return -1;
+        if (a.role === 'student' && b.role === 'other') return -1;
+        return 1;
+      });
+    case 3:
+      return sortedUsers.sort((a, b) => {
+        if (a.active === b.active) return 0;
+        if (a.active && !b.active) return -1;
+        return 1;
+      });
+
     default:
       return sortedUsers;
   }
