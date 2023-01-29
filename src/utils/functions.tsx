4c05abcbe789509807ccc8c7c784618a4b7c730b
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-29 09:13:40 +0100

    Delete paintUsers function

--- src/utils/functions.tsx
+++ src/utils/functions.tsx
@@ -1,13 +1,5 @@
 import { User } from '../types/user.type';
 
-import UserRow from '../components/user-row';
-
-export const paintUsers = (users: User[]) => {
-  if (users.length <= 0) return <p>No hay usuarios</p>;
-
-  return users.map(user => <UserRow key={user.username} {...user} />);
-};
-
 export const filterUsersByName = (users: User[], search: string) => {
   if (!search) return [...users];
 
