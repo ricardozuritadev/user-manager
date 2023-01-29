4dfbcdbb60ad152bd4fea74cdd533bbd97096431
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-29 11:45:27 +0100

    Implement status icons components

--- src/components/user-status.tsx
+++ src/components/user-status.tsx
@@ -1,9 +1,22 @@
+import CheckCircleIcon from './icons/check-circle-icon';
+import CrossCircleIcon from './icons/cross-circle-icon';
+
 type UserStatusProps = {
   active: boolean;
 };
 
 const UserStatus = ({ active }: UserStatusProps) => {
-  return <div>{active ? 'Activo' : 'Inactivo'}</div>;
+  // const activeClassname =
+  const Icon = active ? CheckCircleIcon : CrossCircleIcon;
+
+  return (
+    <div className={`c-user-status c-user-status--${active}`}>
+      <Icon className="c-user-status__icon" />
+      <span className="c-user-status__text">
+        {active ? 'Activo' : 'Inactivo'}
+      </span>
+    </div>
+  );
 };
 
 export default UserStatus;
