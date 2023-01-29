ba6134e724328661077e7184e2f1748a5e0e442d
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-29 19:36:10 +0100

    Create CheckIcon component

---
+++ src/components/icons/check-icon.tsx
@@ -0,0 +1,21 @@
+type CheckIconProps = {
+  className: string;
+};
+
+const CheckIcon = ({ className }: CheckIconProps) => (
+  <svg
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M5 13l4 4L19 7"
+    ></path>
+  </svg>
+);
+export default CheckIcon;
