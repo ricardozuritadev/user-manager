a32bc6cbe96904e2dd822cf4a21507fcb8f1539a
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-29 09:12:15 +0100

    Create UserRowProps

--- src/components/user-row.tsx
+++ src/components/user-row.tsx
@@ -3,21 +3,32 @@ import { User } from '../types/user.type';
 import UserRole from './user-role';
 import UserStatus from './user-status';
 
-const UserRow = ({ username, name, active, role }: User) => {
-  <div className="c-user-row">
-    <div className="c-user-row__name">
-      <span>{name}</span>
+type UserRowProps = {
+  user: User;
+  toggleUserActive: (userId: number) => void;
+};
+
+const UserRow = ({ user, toggleUserActive }: UserRowProps) => {
+  const { id, name, active, role } = user;
+
+  return (
+    <div className="c-user-row">
+      <div className="c-user-row__name">
+        <span>{name}</span>
+      </div>
+      <div className="c-user-row__status">
+        <UserStatus active={active} />
+      </div>
+      <div className="c-user-row__role">
+        <UserRole role={role} />
+      </div>
+      <div className="c-user-row__action">
+        <button onClick={() => toggleUserActive(id)}>
+          {active ? 'Desactivar' : 'Activar'}
+        </button>
+      </div>
     </div>
-    <div className="c-user-row__status">
-      <UserStatus active={active} />
-    </div>
-    <div className="c-user-row__role">
-      <UserRole role={role} />
-    </div>
-    <div className="c-user-row__action">
-      <button onClick={() => {}}>{active ? 'Desactivar' : 'Activar'}</button>
-    </div>
-  </div>;
+  );
 };
 
 export default UserRow;
