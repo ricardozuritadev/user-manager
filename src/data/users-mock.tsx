ba66d3143a0e785513710a4a7269f8366d090776
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-29 09:12:59 +0100

    Add id property

--- src/data/users-mock.tsx
+++ src/data/users-mock.tsx
@@ -2,18 +2,21 @@ import { Role } from '../types/user.type';
 
 export const USERS = [
   {
+    id: 0,
     username: 'pablo',
     name: 'Pablo Castellanos',
     active: true,
     role: Role.teacher,
   },
   {
+    id: 1,
     username: 'jose',
     name: 'Jose Fernándex',
     active: true,
     role: Role.teacher,
   },
   {
+    id: 2,
     username: 'javier',
     name: 'Javier López',
     active: false,
