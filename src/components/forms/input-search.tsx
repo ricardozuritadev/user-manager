587fdbc0c77f5bb0424358519c73a8ee87eabcee
ricardozuritadev <ricardo.zurita.dev@gmail.com>
2023-01-29 18:57:50 +0100

    Modify input search component

--- src/components/forms/input-search.tsx
+++ src/components/forms/input-search.tsx
@@ -1,20 +1,31 @@
 import { ChangeEvent } from 'react';
 
+import SearchIcon from '../icons/search-icon';
+
 type InputSearchProps = {
   placeholder: string;
   value: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
 };
 
-const InputSearch = ({ placeholder, value, onChange }: InputSearchProps) => {
+const InputSearch = ({
+  placeholder,
+  value,
+  onChange,
+  className,
+}: InputSearchProps) => {
   return (
-    <input
-      className="c-input-search"
-      type="text"
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    />
+    <div className={`c-input-search__container ${className || ''}`}>
+      <SearchIcon className="c-input-search__icon" />
+      <input
+        className="c-input-search"
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
   );
 };
 
